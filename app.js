5b5c320f3eedfdd38405d74e1792a13782ba6b50
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-05-04 22:50:01 -0600

    started to set up button

---
+++ app.js
@@ -0,0 +1,8 @@
+//just a test to see what i remember of JS from a boot camp
+
+let button = document.querySelector(button);
+let message = document.querySelector('#message');
+
+function pressButton() {
+    button.addEventListener('onclick', changeColor)
+};
